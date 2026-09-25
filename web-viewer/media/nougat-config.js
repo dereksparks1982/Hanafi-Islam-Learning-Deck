2e5de927debb6ddfc058271bcdf07c09f63dfe99
Abdullāh ibn al-Malik al-Jazzār <dereksparks1982@gmail.com>
2026-09-25 11:27:25 -0500

    Enable Ten Commandments public media test

--- web-viewer/media/nougat-config.js
+++ web-viewer/media/nougat-config.js
@@ -1,9 +1,8 @@
 (() => {
   "use strict";
 
-  // Public media playback is intentionally disabled while the library is reviewed.
   window.HANAFI_NOUGAT_MEDIA = Object.freeze({
-    enabled: false,
-    baseUrl: ""
+    enabled: true,
+    baseUrl: "https://97.201.65.144"
   });
 })();
