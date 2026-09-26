a6f3c7b5f8e8bd89c43f477f97deec358c32e804
Abdullāh ibn al-Malik al-Jazzār <dereksparks1982@gmail.com>
2026-09-26 10:10:39 -0500

    Revert failed Xbox LAN routing candidate

--- web-viewer/media/nougat-config.js
+++ web-viewer/media/nougat-config.js
@@ -3,7 +3,6 @@
 
   window.HANAFI_NOUGAT_MEDIA = Object.freeze({
     enabled: true,
-    baseUrl: "https://97.201.65.144",
-    xboxBaseUrl: "https://192.168.1.158"
+    baseUrl: "https://97.201.65.144"
   });
 })();
